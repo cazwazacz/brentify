5c37b9c3561e50df21025946190c1f252a59c362
Allan Wazacz <cazwazacz@gmail.com>
2018-01-29 13:53:22 +0000

    Replace random sentences on page when button is pressed

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 var paragraphs = document.getElementsByTagName("p");
+
+function getRandomSentenceIndex(splitParagraph) {
+  return Math.floor(Math.random() * splitParagraph.length);
+}
+
+function insertSentence(splitParagraph, index) {
+  index === 0 ? splitParagraph[index] = "YOU HAVE BEEN BRENTIFIED!" : splitParagraph[index] = ". YOU HAVE BEEN BRENTIFIED!";
+  return splitParagraph.join('');
+}
+
 for (var i = 0; i < paragraphs.length; i++) {
-  paragraphs[i].innerHTML = "YOU HAVE BEEN BRENTIFIED!";
+  var splitParagraph = paragraphs[i].innerHTML.split('.');
+  paragraphs[i].innerHTML = insertSentence(splitParagraph, getRandomSentenceIndex(splitParagraph));
 };
