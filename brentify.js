6797e41efdf80084785f64ffaf83e3f39bc9a49f
Allan Wazacz <cazwazacz@gmail.com>
2018-01-29 13:18:22 +0000

    Click button to change all p tags on page When Brentify is clicked, all the inner HTML of the p tags change to 'YOU HAVE BEEN BRENTIFIED!'

--- brentify.js
+++ brentify.js
@@ -1,6 +1,10 @@
 function runScript() {
-  var script = `var paragraphs = document.getElementsByTagName("p");
-  console.log(paragraphs);`
+  var script = `
+  var paragraphs = document.getElementsByTagName("p");
+  for(var i = 0; i < paragraphs.length; i++) {
+    paragraphs[i].innerHTML = "YOU HAVE BEEN BRENTIFIED!";
+  }
+  `
 
   chrome.tabs.executeScript({
     code: script
